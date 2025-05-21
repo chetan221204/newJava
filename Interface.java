a5da12ae2ccc5a32cb1f73ab22cfa209d73b89ec
chetan221204 <chetanchauhan247@gmail.com>
2025-05-21 13:33:47 +0530

    java21may

---
+++ Interface.java
@@ -0,0 +1,20 @@
+         interface Client{
+    void print();
+    void show();
+}
+class Dev implements Client{
+    public void print() {
+        System.out.println("bill");
+    }
+    public void show() {
+        System.out.println("biryaniii");
+    }
+}
+public class Interface{
+    public static void main(String[] args)
+    { 
+        Dev obj = new Dev();
+        obj.show();
+        obj.print();
+    }
+    }
